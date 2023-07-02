c87bab1cc1d748309f206c668b83f44c5516b4e3
SooryaGeorge7 <>
2023-07-02 21:54:50 +0000

    Implemented fxn to fetch data using API

--- restaurants/views.py
+++ restaurants/views.py
@@ -7,3 +7,39 @@ import requests
 
 GOOGLE_PLACES_API_KEY = os.environ.get("GOOGLE_PLACES_API_KEY")
 
+def restaurants(request, category):
+    if category == "asian":
+        
+        url = (
+            f"https://maps.googleapis.com/maps/api/place/textsearch/json?query=Asian%20restaurants%20in%20Dublin&key={GOOGLE_PLACES_API_KEY}"
+        )
+            
+    elif category == "european":
+        url = (
+            f"https://maps.googleapis.com/maps/api/place/textsearch/json?query=European%20restaurants%20in%20Dublin&key={GOOGLE_PLACES_API_KEY}"
+        )
+    elif category == "african":
+        url = (
+            f"https://maps.googleapis.com/maps/api/place/textsearch/json?query=African%20restaurants%20in%20Dublin&key={GOOGLE_PLACES_API_KEY}"
+        )
+    elif category == "irish":
+        url = (
+            f"https://maps.googleapis.com/maps/api/place/textsearch/json?query=Irish%20restaurants%20in%20Dublin&key={GOOGLE_PLACES_API_KEY}"
+        )
+    elif category == "american":
+        url = (
+            f"https://maps.googleapis.com/maps/api/place/textsearch/json?query=American%20restaurants%20in%20Dublin&key={GOOGLE_PLACES_API_KEY}"
+        )
+    elif category == "all":
+        url = (
+            f"https://maps.googleapis.com/maps/api/place/textsearch/json?query=All%20restaurants%20in%20Dublin&key={GOOGLE_PLACES_API_KEY}"
+        )
+    else:
+        pass
+
+    
+    return render(request, "restaurants/categories.html")
+
+    
+
+   
