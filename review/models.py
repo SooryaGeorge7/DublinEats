74ff807ec164c78c82a60873175b9f25c79d71e4
SooryaGeorge7 <>
2023-07-05 22:20:54 +0000

    Added Reviews class in Review.models

--- review/models.py
+++ review/models.py
@@ -9,3 +9,28 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+class Reviews(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(
+        auto_now_add=True,
+        null=True,
+    )
+    Rating = (
+        (1, 'Poor'),
+        (2, 'Fair'),
+        (3, 'Average'),
+        (4, 'Good'),
+        (5, 'Excellent')
+    )
+
+    taste = models.IntegerField(choices=Rating)
+    ambience = models.IntegerField(choices=Rating)
+    customer_service = models.IntegerField(choices=Rating)
+    location = models.IntegerField(choices=Rating)
+    value_for_money = models.IntegerField(choices=Rating)
+
+    def __str__(self):
+        return f"{self.restaurant.name} - {self.user.username}'s Ratings"
+
