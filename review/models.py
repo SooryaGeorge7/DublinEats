49975101e107dcc41434c1505953f866f97b9c99
SooryaGeorge7 <>
2023-07-05 22:18:18 +0000

    Added Restaurant class in Review.models

--- review/models.py
+++ review/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    website = models.URLField(blank=True)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
--- review/views.py
+++ review/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+import os
+
+import requests
+
+
+API_KEY = os.environ.get("API_KEY")
+
+
+def review(request,):
+    
+    return render(request, "review/review.html",)
