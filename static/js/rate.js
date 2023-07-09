6c70dafcdc3485a6ccc7b5e6076a38dd2cec5aef
SooryaGeorge7 <>
2023-07-09 10:12:57 +0000

    Updated js & html for rating functionality

--- static/js/rate.js
+++ static/js/rate.js
@@ -1,100 +1,175 @@
-// get all the stars
-var one = document.getElementById('first');
-var two = document.getElementById('second');
-var three = document.getElementById('third');
-var four = document.getElementById('fourth');
-var five = document.getElementById('fifth');
-
 var form = document.querySelector('.reviewform');
 
-// get the hover stars and add/remove checked class
-var handleSelect = (selection) => {
-    switch (selection) {
-        case 'first': {
-            one.classList.add('checked');
-            two.classList.remove('checked');
-            three.classList.remove('checked');
-            four.classList.remove('checked');
-            five.classList.remove('checked');
-            return;
+var handleRatings = (choice, criteria) => {
+    var stars = document.querySelectorAll(`.${criteria} .btn i`);
 
-        }
-        case 'second': {
-            one.classList.add('checked');
-            two.classList.add('checked');
-            three.classList.remove('checked');
-            four.classList.remove('checked');
-            five.classList.remove('checked');
-            return;
+    stars.forEach(star => star.classList.remove('checked'));
 
-        }
-        case 'third': {
-            one.classList.add('checked');
-            two.classList.add('checked');
-            three.classList.add('checked');
-            four.classList.remove('checked');
-            five.classList.remove('checked');
-            return;
-
-        }
-        case 'fourth': {
-            one.classList.add('checked');
-            two.classList.add('checked');
-            three.classList.add('checked');
-            four.classList.add('checked');
-            five.classList.remove('checked');
-            return;
-
-        }
-        case 'fifth': {
-            one.classList.add('checked');
-            two.classList.add('checked');
-            three.classList.add('checked');
-            four.classList.add('checked');
-            five.classList.add('checked');
-            return;
-
-        }
+    switch (choice) {
+        case 'first':
+            for (var i = 0; i < 1; i++) {
+                stars[i].classList.add('checked');
+            }
+            break;
+        case 'second':
+            for (var i = 0; i < 2; i++) {
+                stars[i].classList.add('checked');
+            }
+            break;
+        case 'third':
+            for (var i = 0; i < 3; i++) {
+                stars[i].classList.add('checked');
+            }
+            break;
+        case 'fourth':
+            for (var i = 0; i < 4; i++) {
+                stars[i].classList.add('checked');
+            }
+            break;
+        case 'fifth':
+            for (var i = 0; i < 5; i++) {
+                stars[i].classList.add('checked');
+            }
+            break;
     }
 };
 
-///Get string value from stars and add numeric value 
-var getNumericValue = (stringValue) => {
-    let numericValue;
+var getNumValue = (stringValue) => {
+    let numValue;
     if (stringValue === 'first') {
-        numericValue = 1;
+        numValue = 1;
     } else if (stringValue === 'second') {
-        numericValue = 2;
+        numValue = 2;
     } else if (stringValue === 'third') {
-        numericValue = 3;
+        numValue = 3;
     } else if (stringValue === 'fourth') {
-        numericValue = 4;
+        numValue = 4;
     } else if (stringValue === 'fifth') {
-        numericValue = 5;
+        numValue = 5;
     } else {
-        numericValue = 0;
+        numValue = 0;
     }
-    return numericValue;
+    return numValue;
 };
 
-var arr = [one, two, three, four, five];
+var criterias = ['taste', 'ambience', 'customer_service', 'location', 'value_for_money'];
 
-///Add event listener to mouse hover on rating stars
-// arr.forEach(item => item.addEventListener('mouseover', (event) => {
-//     handleSelect(event.target.id)
+criterias.forEach(criteria => {
+    var stars = document.querySelectorAll(`.${criteria} .btn i`);
 
-// }))
+    stars.forEach(star => {
+        star.addEventListener('click', (event) => {
+            handleRatings(event.target.id, criteria);
+            var val = event.target.id;
+            var val_num = getNumValue(val);
+            form[criteria].value = val_num;
+        });
+    });
+});
 
-arr.forEach(item => item.addEventListener('click', (event) => {
-    handleSelect(event.target.id);
 
-}));
 
-/// Tranform string value from stars into numeric value 
-arr.forEach(item => item.addEventListener('click', (event) => {
-    var val = event.target.id;
-    var val_num = getNumericValue(val);
-    form.taste.value = val_num;
-}));
+
+
+
+// // get all the stars
+// var one = document.getElementById('first');
+// var two = document.getElementById('second');
+// var three = document.getElementById('third');
+// var four = document.getElementById('fourth');
+// var five = document.getElementById('fifth');
+
+// var form = document.querySelector('.reviewform');
+
+// // get the hover stars and add/remove checked class
+// var handleSelect = (selection) => {
+//     switch (selection) {
+//         case 'first': {
+//             one.classList.add('checked');
+//             two.classList.remove('checked');
+//             three.classList.remove('checked');
+//             four.classList.remove('checked');
+//             five.classList.remove('checked');
+//             return;
+
+//         }
+//         case 'second': {
+//             one.classList.add('checked');
+//             two.classList.add('checked');
+//             three.classList.remove('checked');
+//             four.classList.remove('checked');
+//             five.classList.remove('checked');
+//             return;
+
+//         }
+//         case 'third': {
+//             one.classList.add('checked');
+//             two.classList.add('checked');
+//             three.classList.add('checked');
+//             four.classList.remove('checked');
+//             five.classList.remove('checked');
+//             return;
+
+//         }
+//         case 'fourth': {
+//             one.classList.add('checked');
+//             two.classList.add('checked');
+//             three.classList.add('checked');
+//             four.classList.add('checked');
+//             five.classList.remove('checked');
+//             return;
+
+//         }
+//         case 'fifth': {
+//             one.classList.add('checked');
+//             two.classList.add('checked');
+//             three.classList.add('checked');
+//             four.classList.add('checked');
+//             five.classList.add('checked');
+//             return;
+
+//         }
+//     }
+// };
+
+// ///Get string value from stars and add numeric value 
+// var getNumericValue = (stringValue) => {
+//     let numericValue;
+//     if (stringValue === 'first') {
+//         numericValue = 1;
+//     } else if (stringValue === 'second') {
+//         numericValue = 2;
+//     } else if (stringValue === 'third') {
+//         numericValue = 3;
+//     } else if (stringValue === 'fourth') {
+//         numericValue = 4;
+//     } else if (stringValue === 'fifth') {
+//         numericValue = 5;
+//     } else {
+//         numericValue = 0;
+//     }
+//     return numericValue;
+// };
+
+// var arr = [one, two, three, four, five];
+
+// ///Add event listener to mouse hover on rating stars
+// // arr.forEach(item => item.addEventListener('mouseover', (event) => {
+// //     handleSelect(event.target.id)
+
+// // }))
+
+// arr.forEach(item => item.addEventListener('click', (event) => {
+//     handleSelect(event.target.id);
+
+// }));
+
+// /// Tranform string value from stars into numeric value 
+// arr.forEach(item => item.addEventListener('click', (event) => {
+//     var val = event.target.id;
+//     var val_num = getNumericValue(val);
+//     form.taste.value = val_num;
+    
+// }));
 
 
