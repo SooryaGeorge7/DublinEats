041c8d5d7173e5d34780668ff55277b4c0345b12
SooryaGeorge7 <>
2023-07-17 14:37:10 +0000

    Fix:Updated placeholder in image field

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from cloudinary.models import CloudinaryField
 from review.models import Restaurant
 # Create your models here.
 
-placeholder= (f"https://res.cloudinary.com/dif9bjzee/image/upload/v1688163762/backgroud_hwsqzo.png")
+placeholder= (f"https://res.cloudinary.com/dif9bjzee/image/upload/c_scale,h_50,w_50/v1689590568/profile-gda8c660e4_640_z54qoi.webp")
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -22,7 +22,7 @@ class Profile(models.Model):
     profile_image = CloudinaryField(
         "image",
         default = placeholder,
-        eager=[{"width": 50, "height": 50, "crop": "fill"}],
+        eager=[{"width": 50, "height": 50, "crop": "crop"}],
         transformation={
             "width": 50,
             "height": 50,
