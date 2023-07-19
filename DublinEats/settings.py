7a979a7e04985e4ab0ed44b52c8541091a157819
SooryaGeorge7 <>
2023-07-19 10:48:41 +0000

    Fix:Updated settings for deployment

--- DublinEats/settings.py
+++ DublinEats/settings.py
@@ -27,7 +27,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['8000-sooryageorge7-dublineats-o7pyysdnm7.us2.codeanyapp.com',
                  'dublin-eats-95bc3adbc54d.herokuapp.com', 'localhost']
