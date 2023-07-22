81c773dad9ed3a20a533db86bf962ea219e968d7
SooryaGeorge7 <>
2023-07-22 22:54:14 +0000

    Testing:updated users test_forms

--- users/test_forms.py
+++ users/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .forms import UserSignupForm
+from .forms import UserSignupForm, UserUpdateForm
 
 class UserSignupFormTest(TestCase):
     def test_usersignupform_valid(self):
@@ -54,3 +54,36 @@ class UserSignupFormTest(TestCase):
         self.assertIn("email", form.errors.keys())
         self.assertIn("password1", form.errors.keys())
         self.assertIn("password2", form.errors.keys())
+
+class UserUpdateFormTest(TestCase):
+    def test_userupdateform_valid(self):
+        # Test when form data is valid
+        form_data = {
+            "username": "testuser",
+            "email": "test@example.com",
+            
+        }
+        form = UserUpdateForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_userupdateform_invalid_email(self):
+        # Test when email is invalid
+        form_data = {
+            "username": "testuser",
+            "email": "incorrectemail",
+        }
+        form = UserUpdateForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("email", form.errors.keys())
+        self.assertEqual(form.errors["email"][0], "Enter a valid email address.")
+
+    def test_userupdateform_missing_fields(self):
+        # Test when required fields are missing
+        form_data = {
+            "username": "",
+            "email": "test@example.com",
+        }
+        form = UserUpdateForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("username", form.errors.keys())
+        self.assertEqual(form.errors["username"][0], "This field is required.")
