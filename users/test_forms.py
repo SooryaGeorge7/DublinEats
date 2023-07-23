a124df93e2532117a91264b4dd43aca4a29798e5
SooryaGeorge7 <>
2023-07-23 13:12:20 +0000

    Testing: Added profile form unittest

--- users/test_forms.py
+++ users/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .forms import UserSignupForm, UserUpdateForm
+from .forms import UserSignupForm, UserUpdateForm, ProfileUpdateForm
 
 class UserSignupFormTest(TestCase):
     def test_usersignupform_valid(self):
@@ -88,4 +88,48 @@ class UserUpdateFormTest(TestCase):
         self.assertIn("username", form.errors.keys())
         self.assertEqual(form.errors["username"][0], "This field is required.")
 
-    
+class TestProfileForm(TestCase):
+    def test_ifprofileform_valid(self):
+        form_data = {
+                "firstname": "soorya",
+                "surname": "george",
+                "about": "Foodie until i die",
+                "profile_image": "image.jpg",
+                "fav_food": "indian",
+        }
+        
+        form = ProfileUpdateForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_ifprofileform_invalid(self):
+        
+        form = ProfileUpdateForm(
+            {
+                "firstname": str("a" * 51),
+                "surname": str("a" * 51),
+                "about": str("a" * 201),
+                "fav_food": str("a" * 51),
+            }
+        )
+        self.assertIn("firstname", form.errors.keys())
+        self.assertIn("surname", form.errors.keys())
+        self.assertIn("about", form.errors.keys())
+        self.assertIn("fav_food", form.errors.keys())
+        self.assertEqual(
+            form.errors["firstname"][0],
+            'Ensure this value has at most 50 characters (it has 51).',
+        )
+        self.assertEqual(
+            form.errors["surname"][0],
+            'Ensure this value has at most 50 characters (it has 51).',
+        )
+        self.assertEqual(
+            form.errors["about"][0],
+            'Ensure this value has at most 200 characters (it has 201).',
+        )
+        self.assertEqual(
+            form.errors["fav_food"][0],
+            'Ensure this value has at most 50 characters (it has 51).',
+        )
+        
+        self.assertFalse(form.is_valid())
