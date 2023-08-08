f7016fbe256700094342b092c102e127818ff141
SooryaGeorge7 <>
2023-08-08 22:48:47 +0000

    Refractor:Updated edit profile template

--- review/views.py
+++ review/views.py
@@ -125,7 +125,7 @@ def delete_review(request, restaurant_id, review_id):
         )
         return redirect(reverse(f'{restaurant.category}'))
 
-    profile = Profile.objects.get(user=user)
+    profile = Profile.objects.get(user=review.user)
     if review.restaurant in profile.reviewed.all():
         profile.reviewed.remove(review.restaurant)
 
